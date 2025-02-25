53100354aa0be61900160998a5b897904794dc96
Mame Gor <mamegorciss@5gmail.com>
2025-02-25 10:12:08 +0100

    connexion a la basse de donnée

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// Connexion à la base de données
+$host = "localhost";
+$dbname = "bd_restaurant";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
